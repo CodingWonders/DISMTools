a6ffb793a63ab5d500536377bfda447bd8e5e95e
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2022-12-02 16:15:35 +0100

    Dialog to select parent package names

---
+++ Panels/Img_Ops/Features/PkgParentNames/PkgParentNameLookupDlg.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PkgParentNameLookupDlg
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.OK_Button = New System.Windows.Forms.Button()
+        Me.Cancel_Button = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PackageListerBW = New System.ComponentModel.BackgroundWorker()
+        Me.pkgProc = New System.Diagnostics.Process()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(666, 340)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.OK_Button.Enabled = False
+        Me.OK_Button.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.OK_Button.Location = New System.Drawing.Point(3, 3)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+        Me.OK_Button.TabIndex = 0
+        Me.OK_Button.Text = "OK"
+        '
+        'Cancel_Button
+        '
+        Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel_Button.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
+        Me.Cancel_Button.Name = "Cancel_Button"
+        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+        Me.Cancel_Button.TabIndex = 1
+        Me.Cancel_Button.Text = "Cancel"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(252, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Names of installed packages in the mounted image:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.IntegralHeight = False
+        Me.ListBox1.Location = New System.Drawing.Point(16, 38)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(796, 269)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 316)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(129, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Name of parent package:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(148, 313)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(664, 21)
+        Me.TextBox1.TabIndex = 4
+        '
+        'PackageListerBW
+        '
+        Me.PackageListerBW.WorkerSupportsCancellation = True
+        '
+        'pkgProc
+        '
+        Me.pkgProc.StartInfo.Domain = ""
+        Me.pkgProc.StartInfo.LoadUserProfile = False
+        Me.pkgProc.StartInfo.Password = Nothing
+        Me.pkgProc.StartInfo.StandardErrorEncoding = Nothing
+        Me.pkgProc.StartInfo.StandardOutputEncoding = Nothing
+        Me.pkgProc.StartInfo.UserName = ""
+        Me.pkgProc.SynchronizingObject = Me
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 348)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(192, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Getting package names. Please wait..."
+        Me.Label3.Visible = False
+        '
+        'PkgParentNameLookupDlg
+        '
+        Me.AcceptButton = Me.OK_Button
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Cancel_Button
+        Me.ClientSize = New System.Drawing.Size(824, 381)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "PkgParentNameLookupDlg"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Installed package names"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+    Friend WithEvents Cancel_Button As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents PackageListerBW As System.ComponentModel.BackgroundWorker
+    Friend WithEvents pkgProc As System.Diagnostics.Process
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Panels/Img_Ops/Features/PkgParentNames/PkgParentNameLookupDlg.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Windows.Forms
+Imports System.IO
+Imports System.Text.Encoding
+Imports Microsoft.VisualBasic.ControlChars
+
+Public Class PkgParentNameLookupDlg
+
+    Public pkgSource As String
+    Public pkgArgs As String
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        If TextBox1.Text = "" Then
+            MsgBox("Please specify a package name, and try again.", MsgBoxStyle.OkOnly + MsgBoxStyle.Critical, "Installed package names")
+            Exit Sub
+        ElseIf Not ListBox1.Items.Contains(TextBox1.Text) Then
+            MsgBox("The specified package name does not seem to be in the image. Please specify an available entry, and try again", MsgBoxStyle.OkOnly + MsgBoxStyle.Critical, "Installed package names")
+            Exit Sub
+        Else
+            EnableFeat.TextBox1.Text = TextBox1.Text
+            Me.DialogResult = System.Windows.Forms.DialogResult.OK
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        TextBox1.Text = ListBox1.SelectedItem
+    End Sub
+
+    Private Sub PkgParentNameLookupDlg_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        Label3.Visible = True
+        OK_Button.Enabled = False
+        Cancel_Button.Enabled = False
+        ListBox1.Items.Clear()
+        PackageListerBW.RunWorkerAsync()
+    End Sub
+
+    Private Sub PackageListerBW_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles PackageListerBW.DoWork
+        File.WriteAllText(".\temp.bat",
+                  "@echo off" & CrLf &
+                  "dism /English /image=" & pkgSource & " /get-packages | findstr /c:" & Quote & "Package Identity : " & Quote & " > .\pkgnames")
+        pkgProc.StartInfo.FileName = Environment.GetFolderPath(Environment.SpecialFolder.Windows) & "\system32\cmd.exe"
+        pkgArgs = "/c " & Directory.GetCurrentDirectory() & "\temp.bat"
+        pkgProc.StartInfo.Arguments = pkgArgs
+        pkgProc.StartInfo.CreateNoWindow = True
+        pkgProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        pkgProc.Start()
+        Do Until pkgProc.HasExited
+            If pkgProc.HasExited Then
+                Exit Do
+            End If
+        Loop
+        If Decimal.ToInt32(pkgProc.ExitCode) = 0 Then
+            RemPackage.CheckedListBox1.Items.Clear()
+            RemPackage.CheckedListBox2.Items.Clear()
+            Debug.WriteLine("[INFO] Package names were successfully gathered. The program should return to normal state")
+            Debug.WriteLine("Listing package names:" & CrLf & My.Computer.FileSystem.ReadAllText(".\pkgnames"))
+            Dim pkgNames As New RichTextBox
+            pkgNames.Text = My.Computer.FileSystem.ReadAllText(".\pkgnames")
+            For x = 0 To pkgNames.Lines.Count - 1
+                If pkgNames.Lines(x) = "" Then
+                    Continue For
+                End If
+                ListBox1.Items.Add(pkgNames.Lines(x).Replace("Package Identity : ", "").Trim())
+            Next
+            File.Delete(".\temp.bat")
+            File.Delete(".\pkgnames")
+        Else
+            Debug.WriteLine("[FAIL] Package names were not gathered. Please verify everything's working")
+        End If
+        Label3.Visible = False
+        OK_Button.Enabled = True
+        Cancel_Button.Enabled = True
+    End Sub
+End Class
