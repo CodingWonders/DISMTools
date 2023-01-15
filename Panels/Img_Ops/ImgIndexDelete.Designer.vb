68910c3a177bbb94de65617590740ea4cf0e8824
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-01-15 19:25:23 +0100

    Remove volume images

---
+++ Panels/Img_Ops/ImgIndexDelete.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ImgIndexDelete
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.OK_Button = New System.Windows.Forms.Button()
+        Me.Cancel_Button = New System.Windows.Forms.Button()
+        Me.Win10Title = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Win10Title.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(546, 320)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.OK_Button.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.OK_Button.Location = New System.Drawing.Point(3, 3)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+        Me.OK_Button.TabIndex = 0
+        Me.OK_Button.Text = "OK"
+        '
+        'Cancel_Button
+        '
+        Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel_Button.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
+        Me.Cancel_Button.Name = "Cancel_Button"
+        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+        Me.Cancel_Button.TabIndex = 1
+        Me.Cancel_Button.Text = "Cancel"
+        '
+        'Win10Title
+        '
+        Me.Win10Title.BackColor = System.Drawing.Color.White
+        Me.Win10Title.Controls.Add(Me.PictureBox1)
+        Me.Win10Title.Controls.Add(Me.Label1)
+        Me.Win10Title.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Win10Title.Location = New System.Drawing.Point(0, 0)
+        Me.Win10Title.Name = "Win10Title"
+        Me.Win10Title.Size = New System.Drawing.Size(704, 48)
+        Me.Win10Title.TabIndex = 6
+        Me.Win10Title.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Image = Global.DISMTools.My.Resources.Resources.delete_imgindex
+        Me.PictureBox1.Location = New System.Drawing.Point(660, 8)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(32, 32)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(241, 30)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Remove a volume image"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Source image:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(93, 55)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(380, 21)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button1.Location = New System.Drawing.Point(617, 54)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Browse..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button2.Location = New System.Drawing.Point(479, 54)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Use mounted image"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListView2)
+        Me.GroupBox1.Controls.Add(Me.ListView1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 82)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(680, 232)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Volume images"
+        '
+        'ListView2
+        '
+        Me.ListView2.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView2.Enabled = False
+        Me.ListView2.Location = New System.Drawing.Point(343, 48)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(321, 154)
+        Me.ListView2.TabIndex = 0
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        Me.ListView2.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Index"
+        Me.ColumnHeader3.Width = 41
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Image name"
+        Me.ColumnHeader4.Width = 254
+        '
+        'ListView1
+        '
+        Me.ListView1.CheckBoxes = True
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(16, 48)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(321, 154)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Index"
+        Me.ColumnHeader1.Width = 41
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Image name"
+        Me.ColumnHeader2.Width = 254
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 208)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(230, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Getting indexes from the image. Please wait..."
+        Me.Label4.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 24)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(532, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Please mark the volume images to delete on the left. The image will then have the" & _
+    " indexes shown on the right"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 327)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(129, 17)
+        Me.CheckBox1.TabIndex = 11
+        Me.CheckBox1.Text = "Check image integrity"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "WIM files|*.wim"
+        Me.OpenFileDialog1.Title = "Specify the source image to remove volume images from"
+        '
+        'ImgIndexDelete
+        '
+        Me.AcceptButton = Me.OK_Button
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Cancel_Button
+        Me.ClientSize = New System.Drawing.Size(704, 361)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Win10Title)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "ImgIndexDelete"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Remove a volume image"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Win10Title.ResumeLayout(False)
+        Me.Win10Title.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+    Friend WithEvents Cancel_Button As System.Windows.Forms.Button
+    Friend WithEvents Win10Title As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Panels/Img_Ops/ImgIndexDelete.vb
@@ -0,0 +1,242 @@
+﻿Imports System.Windows.Forms
+Imports System.IO
+Imports System.Text.Encoding
+Imports Microsoft.VisualBasic.ControlChars
+
+Public Class ImgIndexDelete
+
+    Public IndexPositions(65535) As String
+    Public IndexNames(65535) As String
+
+    Public IndexRemovalNames(65535) As String
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Array.Clear(IndexRemovalNames, 0, IndexRemovalNames.Last)
+        Dim imageCount As Integer = ListView1.CheckedItems.Count
+        ' Detect whether volume indexes have been marked for removal
+        If ListView1.CheckedItems.Count <= 0 Then
+            MsgBox("Please select volume images to remove from this image, and try again.", vbOKOnly + vbCritical, "Remove a volume image")
+            Exit Sub
+        End If
+        ProgressPanel.imgIndexDeletionSourceImg = TextBox1.Text
+        ' Detect whether image is mounted
+        ProgressPanel.imgIndexDeletionUnmount = False
+        If MainForm.MountedImageImgFiles.Contains(TextBox1.Text) Then
+            If MsgBox("The program has detected that this image is mounted. In order to remove volume images from a file, it needs to be unmounted. You can remount it later, if you want." & vbCrLf & vbCrLf & "Do note that this will unmount the image without saving changes. Make sure all your changes have been saved before proceeding." & vbCrLf & vbCrLf & "Do you want to unmount this image?", vbYesNo + vbExclamation, "Remove a volume image") = MsgBoxResult.Yes Then
+                Try
+                    For x = 0 To Array.LastIndexOf(MainForm.MountedImageImgFiles, MainForm.MountedImageImgFiles.Last)
+                        If MainForm.MountedImageImgFiles(x) = TextBox1.Text Then
+                            ProgressPanel.imgIndexDeletionUnmount = True
+                            ProgressPanel.UMountImgIndex = MainForm.MountedImageImgIndexes(x)
+                            If MainForm.MountedImageMountDirs(x) = MainForm.MountDir Then ProgressPanel.UMountLocalDir = True Else ProgressPanel.UMountLocalDir = False
+                            ProgressPanel.MountDir = MainForm.MountedImageMountDirs(x)
+                            ProgressPanel.UMountOp = 1
+                            Exit For
+                        End If
+                    Next
+                Catch ex As Exception
+                    Exit Try
+                End Try
+            Else
+                Exit Sub
+            End If
+        End If
+        For x = 0 To ListView1.CheckedItems.Count - 1
+            IndexRemovalNames(x) = ListView1.CheckedItems(x).SubItems(1).Text
+        Next
+        For x = 0 To IndexRemovalNames.Length - 1
+            ProgressPanel.imgIndexDeletionNames(x) = IndexRemovalNames(x)
+        Next
+        ProgressPanel.imgIndexDeletionLastName = ListView1.CheckedItems(imageCount - 1).SubItems(1).Text.Replace("{ListViewSubItem: {", "").Trim().Replace("}}", "").Trim()
+        imageCount = ListView1.CheckedItems.Count
+        ProgressPanel.imgIndexDeletionCount = imageCount
+        If CheckBox1.Checked Then
+            ProgressPanel.imgIndexDeletionIntCheck = True
+        Else
+            ProgressPanel.imgIndexDeletionIntCheck = False
+        End If
+        ProgressPanel.OperationNum = 9
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Visible = False
+        ProgressPanel.ShowDialog(MainForm)
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub ImgIndexDelete_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Computer.Info.OSFullName.Contains("Windows 10") Or My.Computer.Info.OSFullName.Contains("Windows 11") Then
+            Text = ""
+            Win10Title.Visible = True
+        End If
+        If MainForm.SourceImg = "N/A" Then Button2.Enabled = False Else Button2.Enabled = True
+        If MainForm.BackColor = Color.FromArgb(48, 48, 48) Then
+            Win10Title.BackColor = Color.FromArgb(48, 48, 48)
+            BackColor = Color.FromArgb(31, 31, 31)
+            ForeColor = Color.White
+            TextBox1.BackColor = Color.FromArgb(31, 31, 31)
+            ListView1.BackColor = Color.FromArgb(31, 31, 31)
+            ListView2.BackColor = Color.FromArgb(31, 31, 31)
+        ElseIf MainForm.BackColor = Color.FromArgb(239, 239, 242) Then
+            Win10Title.BackColor = Color.White
+            BackColor = Color.FromArgb(238, 238, 242)
+            ForeColor = Color.Black
+            TextBox1.BackColor = Color.FromArgb(238, 238, 242)
+            ListView1.BackColor = Color.FromArgb(238, 238, 242)
+            ListView2.BackColor = Color.FromArgb(238, 238, 242)
+        End If
+        TextBox1.ForeColor = ForeColor
+        GroupBox1.ForeColor = ForeColor
+        ListView1.ForeColor = ForeColor
+        ListView2.ForeColor = ForeColor
+
+        ' Set disabled ListView's backcolor. Source: https://stackoverflow.com/questions/17461902/changing-background-color-of-listview-c-sharp-when-disabled
+        Dim bm As New Bitmap(ListView2.ClientSize.Width, ListView2.ClientSize.Height)
+        Graphics.FromImage(bm).Clear(ListView2.BackColor)
+        ListView2.BackgroundImage = bm
+    End Sub
+
+    Sub GetImageIndexInfo(SourceImage As String)
+        RemoveHandler ListView1.ItemChecked, AddressOf ListView1_ItemChecked
+        ' Clear arrays
+        Array.Clear(IndexNames, 0, IndexNames.Length)
+        Array.Clear(IndexPositions, 0, IndexPositions.Length)
+        ListView1.Items.Clear()
+        ListView2.Items.Clear()
+        Label4.Visible = True
+        Dim IndexGetter As New Process
+        IndexGetter.StartInfo.FileName = Environment.GetFolderPath(Environment.SpecialFolder.Windows) & "\system32\cmd.exe"
+        IndexGetter.StartInfo.CreateNoWindow = True
+        IndexGetter.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        Directory.CreateDirectory(".\tempinfo")
+        ' First, detect if the image has more than 1 index
+        Try
+            File.WriteAllText(".\bin\exthelpers\indexnumbers.bat", _
+                              "@echo off" & CrLf & _
+                              "dism /english /get-imageinfo /imagefile=" & SourceImage & " | find /c " & Quote & "Index : " & Quote & " > .\tempinfo\indexcount")
+        Catch ex As Exception
+            MsgBox("Could not get image indexes. Reason: " & ex.Message, vbOKOnly + vbCritical, "Remove a volume image")
+            Label4.Visible = False
+            Exit Sub
+        End Try
+        IndexGetter.StartInfo.Arguments = "/c " & Directory.GetCurrentDirectory() & "\bin\exthelpers\indexnumbers.bat"
+        IndexGetter.Start()
+        Do Until IndexGetter.HasExited
+            If IndexGetter.HasExited Then
+                Exit Do
+            End If
+        Loop
+        If IndexGetter.ExitCode = 0 Then
+            Dim IndexCount As Integer = CInt(My.Computer.FileSystem.ReadAllText(".\tempinfo\indexcount"))
+            File.Delete(".\bin\exthelpers\indexnumbers.bat")
+            File.Delete(".\tempinfo\indexcount")
+            If IndexCount = 1 Then
+                MsgBox("This image only contains 1 index. You can't remove volume images from this file", vbOKOnly + vbExclamation, "Remove a volume image")
+                Label4.Visible = False
+                Exit Sub
+            End If
+        End If
+        Try
+            File.WriteAllText(".\bin\exthelpers\indexes.bat", _
+                              "@echo off" & CrLf & _
+                              "dism /english /get-imageinfo /imagefile=" & SourceImage & " | findstr /c:" & Quote & "Index : " & Quote & " > .\tempinfo\indexnums" & CrLf & _
+                              "dism /english /get-imageinfo /imagefile=" & SourceImage & " | findstr /c:" & Quote & "Name : " & Quote & " > .\tempinfo\indexnames", _
+                              ASCII)
+        Catch ex As Exception
+            MsgBox("Could not get image indexes. Reason: " & ex.Message, vbOKOnly + vbCritical, "Remove a volume image")
+            Label4.Visible = False
+            Exit Sub
+        End Try
+        IndexGetter.StartInfo.Arguments = "/c " & Directory.GetCurrentDirectory() & "\bin\exthelpers\indexes.bat"
+        IndexGetter.Start()
+        Do Until IndexGetter.HasExited
+            If IndexGetter.HasExited Then
+                Exit Do
+            End If
+        Loop
+        Dim FileGetterRTB As New RichTextBox()
+        Dim TypeLookups() As String = New String(1) {"Index : ", "Name : "}
+        Dim lineToAppend As String = ""
+        If IndexGetter.ExitCode = 0 Then
+            For Each indexFile In My.Computer.FileSystem.GetFiles(".\tempinfo", FileIO.SearchOption.SearchTopLevelOnly)
+                FileGetterRTB.Clear()
+                If Path.GetFileName(indexFile).StartsWith("index") Then
+                    FileGetterRTB.Text = My.Computer.FileSystem.ReadAllText(indexFile)
+                    For x = 0 To FileGetterRTB.Lines.Count - 1
+                        If FileGetterRTB.Lines(x) = "" Then
+                            Continue For
+                        Else
+                            If FileGetterRTB.Lines(x).StartsWith(TypeLookups(0)) Then
+                                lineToAppend = FileGetterRTB.Lines(x).Replace(TypeLookups(0), "").Trim()
+                                IndexPositions(x) = CInt(lineToAppend)
+                            ElseIf FileGetterRTB.Lines(x).StartsWith(TypeLookups(1)) Then
+                                lineToAppend = FileGetterRTB.Lines(x).Replace(TypeLookups(1), "").Trim()
+                                IndexNames(x) = lineToAppend
+                            End If
+                        End If
+                    Next
+                End If
+            Next
+        End If
+        ' IndexPositions and IndexNames go hand in hand
+        Try
+            For x = 0 To Array.LastIndexOf(IndexPositions, IndexPositions.Last)
+                If IndexPositions(x) = "" Or IndexPositions(x) = Nothing Then
+                    Exit For
+                Else
+                    ListView1.Items.Add(New ListViewItem(New String() {IndexPositions(x), IndexNames(x)}))
+                    ListView2.Items.Add(New ListViewItem(New String() {IndexPositions(x), IndexNames(x)}))
+                End If
+            Next
+        Catch ex As Exception
+            Exit Sub
+        End Try
+        Label4.Visible = False
+        AddHandler ListView1.ItemChecked, AddressOf ListView1_ItemChecked
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If File.Exists(TextBox1.Text) Then
+            If Path.GetExtension(TextBox1.Text).Equals(".wim", StringComparison.OrdinalIgnoreCase) Or _
+                Path.GetExtension(TextBox1.Text).Equals(".esd", StringComparison.OrdinalIgnoreCase) Or _
+                Path.GetExtension(TextBox1.Text).Equals(".vhd", StringComparison.OrdinalIgnoreCase) Or _
+                Path.GetExtension(TextBox1.Text).Equals(".vhdx", StringComparison.OrdinalIgnoreCase) Then
+                GetImageIndexInfo(TextBox1.Text)
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = MainForm.SourceImg
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        TextBox1.Text = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub ListView1_ItemChecked(sender As Object, e As ItemCheckedEventArgs) Handles ListView1.ItemChecked
+        ListView2.Items.Clear()
+        Try
+            For x = 0 To ListView1.Items.Count - 1
+                If ListView1.Items(x).Checked Then
+                    Continue For
+                Else
+                    If CInt(ListView1.Items(x).Text) - 1 < 0 Then
+                        ListView2.Items.Add(New ListViewItem(New String() {CInt(ListView1.Items(x).Text) + 1, ListView1.Items(x).SubItems(1).Text}))
+                    Else
+                        ListView2.Items.Add(New ListViewItem(New String() {ListView1.Items(x).Text, ListView1.Items(x).SubItems(1).Text}))
+                    End If
+                End If
+            Next
+        Catch ex As Exception
+            Exit Sub
+        End Try
+    End Sub
+End Class
