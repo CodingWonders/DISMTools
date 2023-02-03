2a18ca80c09fe4ac647d6c188a98e774c5502679
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-02-03 19:12:34 +0100

    Revert "Update AssemblyInfo.vb"
    This reverts commit 41de2f1bd9ecfb1103dd66fc5fab2e46608efb34.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -8,7 +8,7 @@ Imports System.Runtime.InteropServices
 
 ' Revisar los valores de los atributos del ensamblado
 
-<Assembly: AssemblyTitle("DISMTools")> 
+<Assembly: AssemblyTitle("DISMTools: an automation tool and a GUI for DISM operations")> 
 <Assembly: AssemblyDescription("An automation tool for DISM operations")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("DISMTools")> 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.2.1.0")> 
-<Assembly: AssemblyFileVersion("0.2.1.0")> 
+<Assembly: AssemblyVersion("0.2.0.0")> 
+<Assembly: AssemblyFileVersion("0.2.0.0")> 
