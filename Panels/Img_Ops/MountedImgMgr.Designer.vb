64a3a23109f3beea8ee4210bddef8f956c44bbe4
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-02-14 20:44:37 +0100

    Mounted image manager

---
+++ Panels/Img_Ops/MountedImgMgr.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MountedImgMgr
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MountedImgMgr))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ActionsTLP = New System.Windows.Forms.TableLayoutPanel()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.DetectorBW = New System.ComponentModel.BackgroundWorker()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.ActionsTLP.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(983, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.67347!))
+        Me.TableLayoutPanel1.Controls.Add(Me.ListView1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.ActionsTLP, 0, 1)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(16, 52)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(980, 430)
+        Me.TableLayoutPanel1.TabIndex = 2
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6})
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(3, 3)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(974, 389)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Image file"
+        Me.ColumnHeader1.Width = 299
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Index"
+        Me.ColumnHeader2.Width = 40
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Mount directory"
+        Me.ColumnHeader3.Width = 299
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Status"
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Read/write permissions?"
+        Me.ColumnHeader5.Width = 133
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Version"
+        Me.ColumnHeader6.Width = 139
+        '
+        'ActionsTLP
+        '
+        Me.ActionsTLP.ColumnCount = 6
+        Me.ActionsTLP.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.ActionsTLP.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.ActionsTLP.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.ActionsTLP.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.ActionsTLP.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.ActionsTLP.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.ActionsTLP.Controls.Add(Me.Button6, 5, 0)
+        Me.ActionsTLP.Controls.Add(Me.Button4, 3, 0)
+        Me.ActionsTLP.Controls.Add(Me.Button3, 2, 0)
+        Me.ActionsTLP.Controls.Add(Me.Button2, 1, 0)
+        Me.ActionsTLP.Controls.Add(Me.Button5, 4, 0)
+        Me.ActionsTLP.Controls.Add(Me.Button1, 0, 0)
+        Me.ActionsTLP.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ActionsTLP.Location = New System.Drawing.Point(3, 398)
+        Me.ActionsTLP.Name = "ActionsTLP"
+        Me.ActionsTLP.RowCount = 1
+        Me.ActionsTLP.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.ActionsTLP.Size = New System.Drawing.Size(974, 29)
+        Me.ActionsTLP.TabIndex = 1
+        '
+        'Button6
+        '
+        Me.Button6.AutoEllipsis = True
+        Me.Button6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button6.Enabled = False
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button6.Location = New System.Drawing.Point(813, 3)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(158, 23)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Load into project"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button4.Enabled = False
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button4.Location = New System.Drawing.Point(489, 3)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(156, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Open mount directory"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.AutoEllipsis = True
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button3.Enabled = False
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button3.Location = New System.Drawing.Point(327, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(156, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Enable write permissions"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.AutoEllipsis = True
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button2.Enabled = False
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button2.Location = New System.Drawing.Point(165, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(156, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Reload servicing"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.AutoEllipsis = True
+        Me.Button5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button5.Enabled = False
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button5.Location = New System.Drawing.Point(651, 3)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(156, 23)
+        Me.Button5.TabIndex = 1
+        Me.Button5.Text = "Remove volume images..."
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.AutoEllipsis = True
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button1.Enabled = False
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(156, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Unmount image"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LinkLabel1.LinkArea = New System.Windows.Forms.LinkArea(77, 34)
+        Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.DodgerBlue
+        Me.LinkLabel1.Location = New System.Drawing.Point(16, 489)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(980, 63)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "This view is being updated in real time, which may cause a higher CPU usage. Chan" & _
+    "ge image detection settings..."
+        Me.LinkLabel1.UseCompatibleTextRendering = True
+        '
+        'DetectorBW
+        '
+        Me.DetectorBW.WorkerReportsProgress = True
+        Me.DetectorBW.WorkerSupportsCancellation = True
+        '
+        'MountedImgMgr
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 561)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(800, 600)
+        Me.Name = "MountedImgMgr"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mounted image manager"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ActionsTLP.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ActionsTLP As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents ColumnHeader6 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents DetectorBW As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Panels/Img_Ops/MountedImgMgr.vb
@@ -0,0 +1,165 @@
+﻿Imports System.IO
+Public Class MountedImgMgr
+
+    Public ignoreRepeats As Boolean = False
+
+    Private Sub MountedImgMgr_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        If MainForm.BackColor = Color.FromArgb(48, 48, 48) Then
+            BackColor = Color.FromArgb(31, 31, 31)
+            ForeColor = Color.White
+        ElseIf MainForm.BackColor = Color.FromArgb(239, 239, 242) Then
+            BackColor = Color.FromArgb(238, 238, 242)
+            ForeColor = Color.Black
+        End If
+        ListView1.BackColor = BackColor
+        ListView1.ForeColor = ForeColor
+        ListView1.Items.Clear()
+        DetectorBW.RunWorkerAsync()
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Options.TabControl1.SelectedIndex = 8
+        Options.ShowDialog(MainForm)
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+        ' Enable buttons according to the image conditions
+        If ListView1.FocusedItem.Text <> "" Then
+            Button1.Enabled = True
+            If MainForm.MountedImageImgStatuses(ListView1.FocusedItem.Index) > 0 Then
+                Button2.Enabled = True
+                Select Case MainForm.MountedImageImgStatuses(ListView1.FocusedItem.Index)
+                    Case 1
+                        Button2.Text = "Reload servicing"
+                    Case 2
+                        Button2.Text = "Repair component store"
+                End Select
+            Else
+                Button2.Enabled = False
+            End If
+            IIf(MainForm.MountedImageMountedReWr(ListView1.FocusedItem.Index) = 1, Button3.Enabled = True, Button3.Enabled = False)
+            Button4.Enabled = True
+            Button5.Enabled = True
+            If MainForm.isProjectLoaded And MainForm.MountDir = "N/A" Or Not Directory.Exists(MainForm.MountDir & "\Windows") Then
+                Button6.Enabled = True
+            Else
+                Button6.Enabled = False
+            End If
+        Else
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Directory.Exists(ListView1.FocusedItem.SubItems(2).Text) Then
+            Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.Windows) & "\explorer.exe", ListView1.FocusedItem.SubItems(2).Text)
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim useAlternateMethod As Boolean = False
+        If MainForm.isProjectLoaded Then
+            For x = 0 To ListView1.Columns.Count - 1
+                If ListView1.FocusedItem.SubItems(x).Text = "" Or ListView1.FocusedItem.SubItems(x).Text = Nothing Then
+                    useAlternateMethod = True
+                    Exit For
+                End If
+            Next
+        End If
+        If useAlternateMethod Then
+            Try
+                For x = 0 To Array.LastIndexOf(MainForm.MountedImageMountDirs, MainForm.MountedImageMountDirs.Last)
+                    If MainForm.MountedImageMountDirs(x) = ListView1.FocusedItem.SubItems(2).Text Then
+                        MainForm.MountDir = MainForm.MountedImageMountDirs(x)
+                        MainForm.ImgIndex = MainForm.MountedImageImgIndexes(x)
+                        MainForm.SourceImg = MainForm.MountedImageImgFiles(x)
+                        IIf(MainForm.MountedImageMountedReWr(x) = "Yes", MainForm.isReadOnly = True, MainForm.isReadOnly = False)
+                    End If
+                Next
+            Catch ex As Exception
+                Exit Try
+            End Try
+            MainForm.UpdateProjProperties(True, If(MainForm.isReadOnly, True, False))
+            MainForm.SaveDTProj()
+        Else
+            MainForm.MountDir = ListView1.FocusedItem.SubItems(2).Text
+            MainForm.ImgIndex = ListView1.FocusedItem.SubItems(1).Text
+            MainForm.SourceImg = ListView1.FocusedItem.SubItems(0).Text
+            IIf(ListView1.FocusedItem.SubItems(4).Text = "Yes", MainForm.isReadOnly = False, MainForm.isReadOnly = True)
+            MainForm.UpdateProjProperties(True, If(MainForm.isReadOnly, True, False))
+            MainForm.SaveDTProj()
+        End If
+        Button6.Enabled = False
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ProgressPanel.MountDir = ListView1.FocusedItem.SubItems(2).Text
+        ProgressPanel.OperationNum = 18
+        ProgressPanel.ShowDialog()
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MainForm.ImgUMountPopupCMS.Show(sender, New Point(24, Button1.Height * 0.75))
+    End Sub
+
+    Private Sub DetectorBW_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles DetectorBW.DoWork
+        Dim timer As New Stopwatch
+        Do
+            timer.Start()
+            Do
+                If DetectorBW.CancellationPending Then
+                    timer.Stop()
+                    timer.Reset()
+                    Exit Sub
+                End If
+                If timer.ElapsedMilliseconds >= 100 Then
+                    timer.Stop()
+                    DetectorBW.ReportProgress(0)
+                    timer.Reset()
+                    Exit Do
+                End If
+            Loop
+        Loop
+    End Sub
+
+    Private Sub MountedImgMgr_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        DetectorBW.CancelAsync()
+    End Sub
+
+    Private Sub DetectorBW_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles DetectorBW.ProgressChanged
+        If DetectorBW.CancellationPending Then Exit Sub
+        Try
+            For x = 0 To Array.LastIndexOf(MainForm.MountedImageImgFiles, MainForm.MountedImageImgFiles.Last)
+                If ignoreRepeats Then
+                    If ListView1.Items.Count <> MainForm.MountedImageImgFiles.Length Then
+                        ListView1.Items.Clear()
+                        ignoreRepeats = False
+                        Exit Sub
+                    End If
+                    ' Thanks ChatGPT for providing a little help on this
+                    For Each item As ListViewItem In ListView1.Items
+                        If Not MainForm.MountedImageImgFiles.Contains(item.Text) Then
+                            ListView1.Items.Add(New ListViewItem(New String() {MainForm.MountedImageImgFiles(x), MainForm.MountedImageImgIndexes(x), MainForm.MountedImageMountDirs(x), If(MainForm.MountedImageImgStatuses(x) = 0, "OK", If(MainForm.MountedImageImgStatuses(x) = 1, "Needs Remount", "Invalid")), If(MainForm.MountedImageMountedReWr(x) = 0, "Yes", "No"), If(File.Exists(MainForm.MountedImageMountDirs(x) & "\Windows\System32\ntoskrnl.exe"), FileVersionInfo.GetVersionInfo(MainForm.MountedImageMountDirs(x) & "\Windows\system32\ntoskrnl.exe").ProductVersion, "Could not get version info")}))
+                        End If
+                    Next
+                End If
+                If Not ignoreRepeats Then ListView1.Items.Add(New ListViewItem(New String() {MainForm.MountedImageImgFiles(x), MainForm.MountedImageImgIndexes(x), MainForm.MountedImageMountDirs(x), If(MainForm.MountedImageImgStatuses(x) = 0, "OK", If(MainForm.MountedImageImgStatuses(x) = 1, "Needs Remount", "Invalid")), If(MainForm.MountedImageMountedReWr(x) = 0, "Yes", "No"), If(File.Exists(MainForm.MountedImageMountDirs(x) & "\Windows\System32\ntoskrnl.exe"), FileVersionInfo.GetVersionInfo(MainForm.MountedImageMountDirs(x) & "\Windows\system32\ntoskrnl.exe").ProductVersion, "Could not get version info")}))
+            Next
+            ignoreRepeats = True
+        Catch ex As Exception
+            Exit Try
+        End Try
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ImgIndexDelete.TextBox1.Text = ListView1.FocusedItem.SubItems(0).Text
+        ImgIndexDelete.ShowDialog()
+    End Sub
+End Class
