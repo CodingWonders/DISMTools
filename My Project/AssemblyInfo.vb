8f30308b1e556df0b5f7f46b581c85fd5a5b0e7e
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-02-28 20:07:17 +0100

    Update setting file and program versions

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.2.1.2323")> 
-<Assembly: AssemblyFileVersion("0.2.1.0")> 
+<Assembly: AssemblyVersion("0.2.2.2331")> 
+<Assembly: AssemblyFileVersion("0.2.2.0")> 
