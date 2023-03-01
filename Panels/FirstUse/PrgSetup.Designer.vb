4032e58519dcc06a627b98862579c8703e1c9e1d
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-03-01 22:09:23 +0100

    Update file

--- Panels/FirstUse/PrgSetup.Designer.vb
+++ Panels/FirstUse/PrgSetup.Designer.vb
@@ -23,17 +23,24 @@ Partial Class PrgSetup
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.wndControlPanel = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.btnControlPanel = New System.Windows.Forms.Panel()
         Me.Back_Button = New System.Windows.Forms.Button()
         Me.Next_Button = New System.Windows.Forms.Button()
         Me.Cancel_Button = New System.Windows.Forms.Button()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BodyPanelContainer = New System.Windows.Forms.Panel()
+        Me.WelcomePanel = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.wndControlPanel.SuspendLayout()
-        Me.btnControlPanel.SuspendLayout()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.btnControlPanel.SuspendLayout()
+        Me.BodyPanelContainer.SuspendLayout()
+        Me.WelcomePanel.SuspendLayout()
         Me.SuspendLayout()
         '
         'wndControlPanel
@@ -49,10 +56,42 @@ Partial Class PrgSetup
         Me.wndControlPanel.Size = New System.Drawing.Size(960, 48)
         Me.wndControlPanel.TabIndex = 0
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(8, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "DISMTools"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.DISMTools.My.Resources.Resources.minBox
+        Me.PictureBox2.Location = New System.Drawing.Point(869, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(46, 32)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 0
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.DISMTools.My.Resources.Resources.closebox
+        Me.PictureBox1.Location = New System.Drawing.Point(914, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(46, 32)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
         'btnControlPanel
         '
         Me.btnControlPanel.BackColor = System.Drawing.Color.Transparent
         Me.btnControlPanel.BackgroundImage = Global.DISMTools.My.Resources.Resources.wndPanel_Backdrop
+        Me.btnControlPanel.Controls.Add(Me.Label4)
         Me.btnControlPanel.Controls.Add(Me.Back_Button)
         Me.btnControlPanel.Controls.Add(Me.Next_Button)
         Me.btnControlPanel.Controls.Add(Me.Cancel_Button)
@@ -92,36 +131,58 @@ Partial Class PrgSetup
         Me.Cancel_Button.Text = "Cancel"
         Me.Cancel_Button.UseVisualStyleBackColor = True
         '
-        'PictureBox1
+        'BodyPanelContainer
         '
-        Me.PictureBox1.Image = Global.DISMTools.My.Resources.Resources.closebox
-        Me.PictureBox1.Location = New System.Drawing.Point(914, 0)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(46, 32)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
+        Me.BodyPanelContainer.BackColor = System.Drawing.Color.Transparent
+        Me.BodyPanelContainer.Controls.Add(Me.WelcomePanel)
+        Me.BodyPanelContainer.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BodyPanelContainer.Location = New System.Drawing.Point(0, 48)
+        Me.BodyPanelContainer.Name = "BodyPanelContainer"
+        Me.BodyPanelContainer.Size = New System.Drawing.Size(960, 616)
+        Me.BodyPanelContainer.TabIndex = 2
         '
-        'PictureBox2
+        'WelcomePanel
         '
-        Me.PictureBox2.Image = Global.DISMTools.My.Resources.Resources.minBox
-        Me.PictureBox2.Location = New System.Drawing.Point(869, 0)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(46, 32)
-        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
-        Me.PictureBox2.TabIndex = 0
-        Me.PictureBox2.TabStop = False
+        Me.WelcomePanel.Controls.Add(Me.Label3)
+        Me.WelcomePanel.Controls.Add(Me.Label2)
+        Me.WelcomePanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WelcomePanel.Location = New System.Drawing.Point(0, 0)
+        Me.WelcomePanel.Name = "WelcomePanel"
+        Me.WelcomePanel.Size = New System.Drawing.Size(960, 616)
+        Me.WelcomePanel.TabIndex = 0
         '
-        'Label1
+        'Label2
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(8, 8)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(62, 15)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "DISMTools"
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Variable Display Semib", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(24, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(268, 32)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Welcome to DISMTools"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(56, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(769, 20)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "DISMTools is a free and open-source, project-driven GUI for DISM operations. To b" & _
+    "egin setting things up, click Next"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(12, 17)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(579, 15)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "This is internal content, and is in design phases. You should not call this proce" & _
+    "dure since it won't do anything"
         '
         'PrgSetup
         '
@@ -130,18 +191,24 @@ Partial Class PrgSetup
         Me.BackgroundImage = Global.DISMTools.My.Resources.Resources.dt_bgbranding
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(960, 712)
+        Me.Controls.Add(Me.BodyPanelContainer)
         Me.Controls.Add(Me.btnControlPanel)
         Me.Controls.Add(Me.wndControlPanel)
         Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.White
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "PrgSetup"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "PrgSetup"
         Me.wndControlPanel.ResumeLayout(False)
         Me.wndControlPanel.PerformLayout()
-        Me.btnControlPanel.ResumeLayout(False)
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.btnControlPanel.ResumeLayout(False)
+        Me.btnControlPanel.PerformLayout()
+        Me.BodyPanelContainer.ResumeLayout(False)
+        Me.WelcomePanel.ResumeLayout(False)
+        Me.WelcomePanel.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -153,4 +220,9 @@ Partial Class PrgSetup
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents BodyPanelContainer As System.Windows.Forms.Panel
+    Friend WithEvents WelcomePanel As System.Windows.Forms.Panel
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
 End Class
