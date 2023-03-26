c4eb6c375c2860169783f93c159a418c49084b7f
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-03-26 11:14:16 +0200

    Setting file generation now reflect this version

--- MainForm.vb
+++ MainForm.vb
@@ -2479,7 +2479,7 @@ Public Class MainForm
                     File.Delete(".\settings.ini")
                 End If
                 DTSettingForm.RichTextBox2.Clear()
-                DTSettingForm.RichTextBox2.AppendText("# DISMTools (version 0.2.1) configuration file" & CrLf & CrLf & "[Program]" & CrLf)
+                DTSettingForm.RichTextBox2.AppendText("# DISMTools (version 0.2.2) configuration file" & CrLf & CrLf & "[Program]" & CrLf)
                 DTSettingForm.RichTextBox2.AppendText("DismExe=" & Quote & DismExe & Quote)
                 If SaveOnSettingsIni Then
                     DTSettingForm.RichTextBox2.AppendText(CrLf & "SaveOnSettingsIni=1")
