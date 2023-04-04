571f9b555674979d2799e5d19e9adb33ce9c82d9
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-04-04 12:00:35 +0200

    Add window handling through title bar controls

--- Panels/FirstUse/PrgSetup.vb
+++ Panels/FirstUse/PrgSetup.vb
@@ -1,3 +1,78 @@
 ﻿Public Class PrgSetup
 
+    Dim btnToolTip As New ToolTip()
+    Private isMouseDown As Boolean = False
+    Private mouseOffset As Point
+
+    Private Sub minBox_MouseEnter(sender As Object, e As EventArgs) Handles minBox.MouseEnter
+        minBox.Image = My.Resources.minBox_focus
+    End Sub
+
+    Private Sub minBox_MouseLeave(sender As Object, e As EventArgs) Handles minBox.MouseLeave
+        minBox.Image = My.Resources.minBox
+    End Sub
+
+    Private Sub minBox_MouseDown(sender As Object, e As MouseEventArgs) Handles minBox.MouseDown
+        minBox.Image = My.Resources.minBox_down
+    End Sub
+
+    Private Sub minBox_MouseUp(sender As Object, e As MouseEventArgs) Handles minBox.MouseUp
+        minBox.Image = My.Resources.minBox_focus
+    End Sub
+
+    Private Sub minBox_MouseHover(sender As Object, e As EventArgs) Handles minBox.MouseHover
+        btnToolTip.SetToolTip(sender, "Minimize")
+    End Sub
+
+    Private Sub minBox_Click(sender As Object, e As EventArgs) Handles minBox.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub closeBox_MouseEnter(sender As Object, e As EventArgs) Handles closeBox.MouseEnter
+        closeBox.Image = My.Resources.closebox_focus
+    End Sub
+
+    Private Sub closeBox_MouseLeave(sender As Object, e As EventArgs) Handles closeBox.MouseLeave
+        closeBox.Image = My.Resources.closebox
+    End Sub
+
+    Private Sub closeBox_MouseDown(sender As Object, e As MouseEventArgs) Handles closeBox.MouseDown
+        closeBox.Image = My.Resources.closebox_down
+    End Sub
+
+    Private Sub closeBox_MouseUp(sender As Object, e As MouseEventArgs) Handles closeBox.MouseUp
+        closeBox.Image = My.Resources.closebox_focus
+    End Sub
+
+    Private Sub closeBox_MouseHover(sender As Object, e As EventArgs) Handles closeBox.MouseHover
+        btnToolTip.SetToolTip(sender, "Close")
+    End Sub
+
+    Private Sub closeBox_Click(sender As Object, e As EventArgs) Handles closeBox.Click
+        Close()
+    End Sub
+
+    Private Sub wndControlPanel_MouseDown(sender As Object, e As MouseEventArgs) Handles wndControlPanel.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            ' Get the new position
+            mouseOffset = New Point(-e.X, -e.Y)
+            ' Set that left button is pressed
+            isMouseDown = True
+        End If
+    End Sub
+
+    Private Sub wndControlPanel_MouseMove(sender As Object, e As MouseEventArgs) Handles wndControlPanel.MouseMove
+        If isMouseDown Then
+            Dim mousePos As Point = Control.MousePosition
+            ' Get the new form position
+            mousePos.Offset(mouseOffset.X, mouseOffset.Y)
+            Location = mousePos
+        End If
+    End Sub
+
+    Private Sub wndControlPanel_MouseUp(sender As Object, e As MouseEventArgs) Handles wndControlPanel.MouseUp
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            isMouseDown = False
+        End If
+    End Sub
 End Class
