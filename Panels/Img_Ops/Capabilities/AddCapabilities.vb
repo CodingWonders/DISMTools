8d4c31081c0c28caa0108eef302818c5e954dc6a
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-04-06 21:16:37 +0200

    Add "Add capabilities" dialog

---
+++ Panels/Img_Ops/Capabilities/AddCapabilities.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Windows.Forms
+
+Public Class AddCapabilities
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub AddCapability_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Environment.OSVersion.Version.Major = 10 Then
+            Text = ""
+            Win10Title.Visible = True
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            Label2.Enabled = True
+            TextBox1.Enabled = True
+            Button1.Enabled = True
+        Else
+            Label2.Enabled = False
+            TextBox1.Enabled = False
+            Button1.Enabled = False
+        End If
+    End Sub
+End Class
