32f20d7652290c36e22a510275bf0bdc68444664
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-04-17 20:38:23 +0200

    Initial design

--- Actions/DockPanels/ActionProperties.Designer.vb
+++ Actions/DockPanels/ActionProperties.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class ActionProperties
-    Inherits DarkUI.Docking.DarkToolWindow
+    Inherits WeifenLuo.WinFormsUI.Docking.DockContent
 
     'UserControl reemplaza a Dispose para limpiar la lista de componentes.
     <System.Diagnostics.DebuggerNonUserCode()> _
@@ -22,18 +22,38 @@ Partial Class ActionProperties
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.SuspendLayout()
         '
-        'ToolWindow
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(13, 13)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(97, 19)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'ActionProperties
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.DockText = "Properties"
+        Me.ClientSize = New System.Drawing.Size(224, 321)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.DockAreas = CType(((((WeifenLuo.WinFormsUI.Docking.DockAreas.Float Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockLeft) _
+            Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight) _
+            Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop) _
+            Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom), WeifenLuo.WinFormsUI.Docking.DockAreas)
         Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Name = "ToolWindow"
-        Me.Size = New System.Drawing.Size(175, 173)
+        Me.Name = "ActionProperties"
+        Me.ShowIcon = False
+        Me.Text = "Properties"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
 
 End Class
--- Actions/DockPanels/ActionProperties.vb
+++ Actions/DockPanels/ActionProperties.vb
@@ -1,7 +1,12 @@
 ﻿Partial Public Class ActionProperties
-    Inherits DarkUI.Docking.DarkToolWindow
+    Inherits WeifenLuo.WinFormsUI.Docking.DockContent
 
     Public Sub ActionProperties()
         InitializeComponent()
+    End Sub
+
+    Private Sub ActionProperties_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        BackColor = Color.FromArgb(37, 37, 38)
+        ForeColor = Actions_MainForm.ForeColor
     End Sub
 End Class
