87f083eddb6ebbe8bca27018ba101083cfc1406a
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-04-17 20:38:58 +0200

    Add validation resource

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -1558,6 +1558,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property validation() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("validation", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property ver_stability() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("ver_stability", resourceCulture)
