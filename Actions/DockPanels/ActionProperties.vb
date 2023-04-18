6e678b1f8fb51c6eae77eb36d9252310631b9695
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-04-18 15:24:52 +0200

    Fix color issue

--- Actions/DockPanels/ActionProperties.vb
+++ Actions/DockPanels/ActionProperties.vb
@@ -6,7 +6,7 @@
     End Sub
 
     Private Sub ActionProperties_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
-        BackColor = Color.FromArgb(37, 37, 38)
+        BackColor = If(MainForm.BackColor = Color.FromArgb(48, 48, 48), Color.FromArgb(37, 37, 38), Color.FromArgb(246, 246, 246))
         ForeColor = Actions_MainForm.ForeColor
     End Sub
 End Class
