f44d8cb553a7cb72a705e6ba682461cca036c4fa
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-05-11 09:17:51 +0200

    Don't delete package source directory for new references

--- nugetpkgprep.bat
+++ nugetpkgprep.bat
@@ -1,4 +1,4 @@
 @echo off
 :: Refresh the NuGet packages directory
 if exist .\packages (rd .\packages /s /q)
-if exist .\pkgsrc (move .\pkgsrc .\packages)
+if exist .\pkgsrc (copy .\pkgsrc .\packages)
