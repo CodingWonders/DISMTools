ef327e0749f4449ec4f1b6745a38014bdf6974a8
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-05-27 18:00:24 +0200

    Blow it all and start over

--- MainForm.Designer.vb
+++ MainForm.Designer.vb
@@ -1326,6 +1326,7 @@ Partial Class MainForm
         '
         'ActionEditorToolStripMenuItem
         '
+        Me.ActionEditorToolStripMenuItem.Enabled = False
         Me.ActionEditorToolStripMenuItem.Name = "ActionEditorToolStripMenuItem"
         Me.ActionEditorToolStripMenuItem.Size = New System.Drawing.Size(280, 22)
         Me.ActionEditorToolStripMenuItem.Text = "Action editor"
@@ -2315,7 +2316,7 @@ Partial Class MainForm
         Me.TableLayoutPanel2.ColumnCount = 3
         Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 44.44444!))
         Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55.55556!))
-        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 56.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57.0!))
         Me.TableLayoutPanel2.Controls.Add(Me.ProjNameEditBtn, 2, 0)
         Me.TableLayoutPanel2.Controls.Add(Me.LinkLabel1, 1, 3)
         Me.TableLayoutPanel2.Controls.Add(Me.Panel8, 1, 0)
@@ -2364,7 +2365,7 @@ Partial Class MainForm
         Me.Panel8.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Panel8.Location = New System.Drawing.Point(91, 3)
         Me.Panel8.Name = "Panel8"
-        Me.Panel8.Size = New System.Drawing.Size(105, 23)
+        Me.Panel8.Size = New System.Drawing.Size(104, 23)
         Me.Panel8.TabIndex = 8
         '
         'projName
@@ -2373,7 +2374,7 @@ Partial Class MainForm
         Me.projName.Dock = System.Windows.Forms.DockStyle.Fill
         Me.projName.Location = New System.Drawing.Point(0, 0)
         Me.projName.Name = "projName"
-        Me.projName.Size = New System.Drawing.Size(105, 23)
+        Me.projName.Size = New System.Drawing.Size(104, 23)
         Me.projName.TabIndex = 2
         Me.projName.Text = "projName"
         Me.projName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -2383,7 +2384,7 @@ Partial Class MainForm
         Me.projNameText.Dock = System.Windows.Forms.DockStyle.Fill
         Me.projNameText.Location = New System.Drawing.Point(0, 0)
         Me.projNameText.Name = "projNameText"
-        Me.projNameText.Size = New System.Drawing.Size(105, 21)
+        Me.projNameText.Size = New System.Drawing.Size(104, 21)
         Me.projNameText.TabIndex = 4
         Me.projNameText.Text = "projName"
         Me.projNameText.Visible = False
