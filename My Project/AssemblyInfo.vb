b9d745a9aff2e56bd09f64193af1d0411b00e1e0
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-06-24 15:18:31 +0200

    Update version information and nightly installer link, and add missing license information

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3.0.2364")> 
-<Assembly: AssemblyFileVersion("0.3.0.2364")> 
+<Assembly: AssemblyVersion("0.3.0.2371")> 
+<Assembly: AssemblyFileVersion("0.3.0.2371")> 
