710ad7ec3d4ab6a996174a9619cf5c02d2b0c971
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-12-18 18:25:38 +0100

    Fixed button positioning issues on Windows 8 hosts

--- Panels/Get_Ops/Capabilities/GetCapabilityInfo.Designer.vb
+++ Panels/Get_Ops/Capabilities/GetCapabilityInfo.Designer.vb
@@ -409,6 +409,7 @@ Partial Class GetCapabilityInfoDlg
         '
         'Button2
         '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.Button2.Location = New System.Drawing.Point(848, 499)
         Me.Button2.Name = "Button2"
