bef715bdf099905df694b736bd87cf7969fa6638
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2023-12-23 13:10:33 +0100

    Update release status and branch information

--- MainForm.vb
+++ MainForm.vb
@@ -125,7 +125,7 @@ Public Class MainForm
     Public isSqlServerDTProj As Boolean
 
     ' Set branch name and codenames
-    Public dtBranch As String = "dt_preview"
+    Public dtBranch As String = "stable"
 
     ' Arrays and other variables used on background processes
     Public imgPackageNames(65535) As String
