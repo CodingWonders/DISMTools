45d0dddfb3e26962bee534e6f3b73b6ac1619fe7
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-03-10 10:38:43 +0100

    Add possible fix to freezing issue

---
+++ ApplicationEvents.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.Win32
+
+Namespace My
+    ' Los siguientes eventos están disponibles para MyApplication:
+    ' 
+    ' Inicio: se desencadena cuando se inicia la aplicación, antes de que se cree el formulario de inicio.
+    ' Apagado: generado después de cerrar todos los formularios de la aplicación. Este evento no se genera si la aplicación termina de forma anómala.
+    ' UnhandledException: generado si la aplicación detecta una excepción no controlada.
+    ' StartupNextInstance: se desencadena cuando se inicia una aplicación de instancia única y la aplicación ya está activa. 
+    ' NetworkAvailabilityChanged: se desencadena cuando la conexión de red está conectada o desconectada.
+    Partial Friend Class MyApplication
+
+        Private Sub Start(sender As Object, e As EventArgs) Handles Me.Startup
+            AddHandler Microsoft.Win32.SystemEvents.UserPreferenceChanged, AddressOf SysEvts_UserPreferenceChanged
+            AddHandler Microsoft.Win32.SystemEvents.DisplaySettingsChanging, AddressOf SysEvts_DisplaySettingsChanging
+            AddHandler Microsoft.Win32.SystemEvents.DisplaySettingsChanged, AddressOf SysEvts_DisplaySettingsChanged
+        End Sub
+
+        Private Sub SysEvts_UserPreferenceChanged(sender As Object, e As Microsoft.Win32.UserPreferenceChangedEventArgs)
+            ' Do nothing
+        End Sub
+
+        Private Sub SysEvts_DisplaySettingsChanged(sender As Object, e As EventArgs)
+            ' Do nothing
+        End Sub
+
+        Private Sub SysEvts_DisplaySettingsChanging(sender As Object, e As EventArgs)
+            ' Do nothing
+        End Sub
+
+    End Class
+
+
+End Namespace
+
