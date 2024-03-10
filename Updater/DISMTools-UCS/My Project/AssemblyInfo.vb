cb86cfb45165f00f415f63db1cad4a889ec6449f
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-03-10 10:48:35 +0100

    Don't show update recommendation on nightly versions of DT

--- Updater/DISMTools-UCS/My Project/AssemblyInfo.vb
+++ Updater/DISMTools-UCS/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.5.0")> 
-<Assembly: AssemblyFileVersion("1.0.5.0")> 
+<Assembly: AssemblyVersion("1.0.5.2")> 
+<Assembly: AssemblyFileVersion("1.0.5.2")> 
