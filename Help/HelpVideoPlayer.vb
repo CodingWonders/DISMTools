c58815c158cc112870bd7a44dd3a4ee2c313661d
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-03-19 17:58:12 +0100

    Add video playback support for tutorial videos

---
+++ Help/HelpVideoPlayer.vb
@@ -0,0 +1,13 @@
+﻿Public Class HelpVideoPlayer
+
+    Private Sub HelpVideoPlayer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub HelpVideoPlayer_VisibleChanged(sender As Object, e As EventArgs) Handles MyBase.VisibleChanged
+        If Visible Then
+            Dim handle As IntPtr = MainForm.GetWindowHandle(Me)
+            If MainForm.IsWindowsVersionOrGreater(10, 0, 18362) Then MainForm.EnableDarkTitleBar(handle, MainForm.BackColor = Color.FromArgb(48, 48, 48))
+        End If
+    End Sub
+End Class
