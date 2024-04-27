8eff011ab8582af8b992edd952ef48a5a6812057
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-04-27 09:01:24 +0200

    Add workaround for #114

--- Panels/Get_Ops/AppxPkgs/GetAppxPkgInfo.vb
+++ Panels/Get_Ops/AppxPkgs/GetAppxPkgInfo.vb
@@ -202,7 +202,7 @@ Public Class GetAppxPkgInfoDlg
                     ListBox1.Items.Add(PackageName)
                 Next
                 ' An empty entry will appear, so remove it
-                ListBox1.Items.RemoveAt(ListBox1.Items.Count - 1)
+                If ListBox1.Items.Count > 0 Then ListBox1.Items.RemoveAt(ListBox1.Items.Count - 1)
                 SearchPanel.Visible = False
             Else
                 For Each InstalledAppxPkg As DismAppxPackage In InstalledAppxPkgInfo
@@ -217,7 +217,7 @@ Public Class GetAppxPkgInfoDlg
                 ListBox1.Items.Add(PackageName)
             Next
             ' An empty entry will appear, so remove it
-            ListBox1.Items.RemoveAt(ListBox1.Items.Count - 1)
+            If ListBox1.Items.Count > 0 Then ListBox1.Items.RemoveAt(ListBox1.Items.Count - 1)
             SearchPanel.Visible = False
         End If
         SearchBox1.Text = ""
