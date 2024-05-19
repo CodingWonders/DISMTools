58360f5cfa9b45d1e8833c87650919867b208a2e
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-05-19 11:27:58 +0200

    Get signer information when saving installed driver info to file

--- Panels/Get_Ops/InfoSave/ImgInfoSaveDlg.vb
+++ Panels/Get_Ops/InfoSave/ImgInfoSaveDlg.vb
@@ -1925,6 +1925,7 @@ Public Class ImgInfoSaveDlg
                                 msg(0) = "Obter informações sobre os controladores... (controlador " & InstalledDrvInfo.IndexOf(driver) + 1 & " de " & InstalledDrvInfo.Count & ")"
                         End Select
                         ReportChanges(msg(0), (InstalledDrvInfo.IndexOf(driver) / InstalledDrvInfo.Count) * 100)
+                        Dim signer As String = DriverSignerViewer.GetSignerInfo(driver.OriginalFileName)
                         Contents &= "  Driver " & InstalledDrvInfo.IndexOf(driver) + 1 & " of " & InstalledDrvInfo.Count & ":" & CrLf & _
                                     "    - Published name: " & driver.PublishedName & CrLf & _
                                     "    - Original file name: " & Path.GetFileName(driver.OriginalFileName) & " (" & Path.GetDirectoryName(driver.OriginalFileName) & ")" & CrLf & _
@@ -1937,7 +1938,7 @@ Public Class ImgInfoSaveDlg
                                     "    - Is critical to the boot process? " & If(driver.BootCritical, "Yes", "No") & CrLf & _
                                     "    - Version: " & driver.Version.ToString() & CrLf & _
                                     "    - Date: " & driver.Date & CrLf & _
-                                    "    - Driver signature status: " & Casters.CastDismSignatureStatus(driver.DriverSignature) & CrLf & CrLf
+                                    "    - Driver signature status: " & Casters.CastDismSignatureStatus(driver.DriverSignature) & If(Not (signer Is Nothing OrElse signer = ""), " by " & signer, "") & CrLf & CrLf
                     Next
                     Contents &= "  - Complete driver information has been gathered" & CrLf & CrLf
                 ElseIf (Not SkipQuestions Or Not AutoCompleteInfo(4)) And MsgBox(msg(1), vbYesNo + vbQuestion, msg(2)) = MsgBoxResult.Yes Then
