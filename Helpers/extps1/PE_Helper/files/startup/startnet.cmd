61c6947e7293f3c837897d855a099a30b7913cb5
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-06-25 16:44:13 +0200

    Added the Driver Installation Module (#135)
    * DISMTools Nightly Installer
    
    * Added DIM executable and project source code
    
    * DISMTools Nightly Installer
    
    * Implemented the DIM on PEs
    
    * DISMTools Nightly Installer
    
    * Keep file version parity
    
    * Added DIM driver installation capabilities to the OS installer
    
    * DISMTools Nightly Installer
    
    * Finalized DIM integration
    
    * DISMTools Nightly Installer
    
    ---------
    
    Co-authored-by: CodingWonders <CodingWonders@users.noreply.github.com>

--- Helpers/extps1/PE_Helper/files/startup/startnet.cmd
+++ Helpers/extps1/PE_Helper/files/startup/startnet.cmd
@@ -2,6 +2,7 @@
 setlocal ENABLEDELAYEDEXPANSION
 title DISMTools Preinstallation Environment
 set version=0.5
+set sysdrive=%SYSTEMDRIVE%
 echo DISMTools %version% - Preinstallation Environment
 echo (c) 2024. CodingWonders Software
 echo.
@@ -15,6 +16,12 @@ for %%D in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
         if exist "%%D:\PE_Helper.ps1" (
             echo Starting script in drive %%D:...
             cd /d %%D:
+            if exist "%%D:\Tools\DIM" (
+                echo.
+                echo Copying program tools to the environment...
+                md "%sysdrive%\Tools\DIM"
+                xcopy "%%D:\Tools\DIM\*" "%sysdrive%\Tools\DIM" /cehyi > nul
+            )
             powershell .\PE_Helper.ps1 StartApply
             if %ERRORLEVEL% equ 0 (
                 set FOUND=1
