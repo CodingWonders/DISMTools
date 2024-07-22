bb19644bedebca3d05c381215bc66977cb5d0b35
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-07-22 14:03:29 +0200

    Added checks for images

--- Helpers/extps1/PE_Helper/files/startup/startnet.cmd
+++ Helpers/extps1/PE_Helper/files/startup/startnet.cmd
@@ -9,10 +9,8 @@ echo.
 echo Please wait while the environment starts up...
 wpeinit
 powershell -command Set-ExecutionPolicy Unrestricted
-set FOUND=0
 for %%D in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     if exist "%%D:\" (
-        if %FOUND% equ 1 (goto END)
         if exist "%%D:\PE_Helper.ps1" (
             echo Starting script in drive %%D:...
             cd /d %%D:
@@ -23,12 +21,6 @@ for %%D in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
                 xcopy "%%D:\Tools\DIM\*" "%sysdrive%\Tools\DIM" /cehyi > nul
             )
             powershell .\PE_Helper.ps1 StartApply
-            if %ERRORLEVEL% equ 0 (
-                set FOUND=1
-            )            
         )
     )
 )
-
-:END
-wpeutil reboot
