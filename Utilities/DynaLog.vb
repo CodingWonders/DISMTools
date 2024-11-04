f2b0aff8ef60f4428c8fb9e27308466c5c54ec71
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-11-04 20:22:53 +0100

    [DynaLog] Add caller thread information to logs

--- Utilities/DynaLog.vb
+++ Utilities/DynaLog.vb
@@ -8,15 +8,15 @@ Imports Microsoft.VisualBasic.ControlChars
 Public Class DynaLog
 
     Public Shared Sub BeginLogging()
-        LogMessage("DynaLog Logger has begun logging program operations...")
-        LogMessage("--- Time Stamps are shown in UTC Time!!! ---")
+        LogMessage("DynaLog Logger has begun logging program operations...", False)
+        LogMessage("--- Time Stamps are shown in UTC Time!!! ---", False)
     End Sub
 
     Public Shared Sub EndLogging()
-        LogMessage("DynaLog Logger has stopped logging program operations...")
+        LogMessage("DynaLog Logger has stopped logging program operations...", False)
     End Sub
 
-    Public Shared Sub LogMessage(message As String)
+    Public Shared Sub LogMessage(message As String, Optional GetParentCaller As Boolean = True)
         Debug.WriteLine(message)
         Try
             ' DynaLog will NOT display logs for log file/folder creation - ONLY in debugger.
@@ -27,7 +27,7 @@ Public Class DynaLog
             If File.Exists(Application.StartupPath & "\logs\DT_DynaLog.log") Then
                 Contents = File.ReadAllText(Application.StartupPath & "\logs\DT_DynaLog.log")
             End If
-            Dim MessageLine As String = "[" & Date.UtcNow.ToString("MM/dd/yyyy HH:mm:ss") & "] " & message
+            Dim MessageLine As String = "[" & Date.UtcNow.ToString("MM/dd/yyyy HH:mm:ss") & "] " & "[" & New StackFrame(1).GetMethod().Name & If(GetParentCaller, " (" & New StackFrame(2).GetMethod().Name & ")", "") & "] " & message
             Contents &= If(Contents <> "", CrLf, "") & MessageLine
             File.WriteAllText(Application.StartupPath & "\logs\DT_DynaLog.log", Contents)
         Catch ex As Exception
