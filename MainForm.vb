3ace78a50dbeb1955887e1a859750c13cd1fcfa3
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-11-20 22:09:00 +0100

    [Event] Add AME Playbook detection to output with DynaLog

--- MainForm.vb
+++ MainForm.vb
@@ -591,6 +591,16 @@ Public Class MainForm
         Catch ex As Exception
 
         End Try
+
+        ' Detect presence of verified AME Playbooks in current system. This is NOT a way to block these projects, but a way to help isolate
+        ' specific program bugs on those types of systems
+        If PlaybookDetector.DetectInstalledPlaybook(PlaybookDetector.VerifiedPlaybooks.AtlasOS) Then
+            DynaLog.LogMessage("Atlas OS has been detected on this system. There may be compatibility issues with DISMTools on your system", False)
+        End If
+        If PlaybookDetector.DetectInstalledPlaybook(PlaybookDetector.VerifiedPlaybooks.ReviOS) Then
+            DynaLog.LogMessage("Revision (ReviOS) has been detected on this system. There may be compatibility issues with DISMTools on your system", False)
+        End If
+
         If Not Directory.Exists(Application.StartupPath & "\logs") Then Directory.CreateDirectory(Application.StartupPath & "\logs")
         If Not Debugger.IsAttached Then SplashScreen.Show()
         Thread.Sleep(2000)
