3a67764aefa3ef7c37bd6078890e4b0456b3a569
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-03-13 20:33:28 +0100

    [Info Saver] Severely improve reports, but saying goodbye to a feature

--- Panels/Get_Ops/InfoSave/InfoSaveResults.Designer.vb
+++ Panels/Get_Ops/InfoSave/InfoSaveResults.Designer.vb
@@ -25,12 +25,10 @@ Partial Class InfoSaveResults
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(InfoSaveResults))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
         Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -40,7 +38,7 @@ Partial Class InfoSaveResults
         Me.Label1.AutoEllipsis = True
         Me.Label1.Location = New System.Drawing.Point(13, 13)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(982, 26)
+        Me.Label1.Size = New System.Drawing.Size(1239, 26)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "The report has been saved to the location you had specified, and its contents wil" & _
     "l be shown below."
@@ -49,23 +47,12 @@ Partial Class InfoSaveResults
         '
         Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.Button1.Location = New System.Drawing.Point(919, 526)
+        Me.Button1.Location = New System.Drawing.Point(1176, 646)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "OK"
         Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.Button2.Location = New System.Drawing.Point(13, 526)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Print..."
-        Me.Button2.UseVisualStyleBackColor = True
         '
         'PrintDialog1
         '
@@ -81,7 +68,7 @@ Partial Class InfoSaveResults
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ReadOnly = True
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox1.Size = New System.Drawing.Size(981, 477)
+        Me.TextBox1.Size = New System.Drawing.Size(1238, 597)
         Me.TextBox1.TabIndex = 4
         '
         'CheckBox1
@@ -91,7 +78,7 @@ Partial Class InfoSaveResults
         Me.CheckBox1.Checked = True
         Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
         Me.CheckBox1.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.CheckBox1.Location = New System.Drawing.Point(695, 526)
+        Me.CheckBox1.Location = New System.Drawing.Point(952, 646)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(218, 23)
         Me.CheckBox1.TabIndex = 5
@@ -110,38 +97,24 @@ Partial Class InfoSaveResults
         Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
         Me.WebBrowser1.Name = "WebBrowser1"
         Me.WebBrowser1.ScriptErrorsSuppressed = True
-        Me.WebBrowser1.Size = New System.Drawing.Size(981, 477)
+        Me.WebBrowser1.Size = New System.Drawing.Size(1238, 597)
         Me.WebBrowser1.TabIndex = 6
-        '
-        'Label2
-        '
-        Me.Label2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Label2.AutoEllipsis = True
-        Me.Label2.Location = New System.Drawing.Point(94, 531)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(595, 13)
-        Me.Label2.TabIndex = 7
-        Me.Label2.Text = "When printing, set orientation to landscape for a better viewing experience"
         '
         'InfoSaveResults
         '
         Me.AcceptButton = Me.Button1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1007, 561)
-        Me.Controls.Add(Me.Label2)
+        Me.ClientSize = New System.Drawing.Size(1264, 681)
         Me.Controls.Add(Me.WebBrowser1)
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MinimumSize = New System.Drawing.Size(800, 600)
+        Me.MinimumSize = New System.Drawing.Size(512, 600)
         Me.Name = "InfoSaveResults"
-        Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Image information report results"
         Me.ResumeLayout(False)
@@ -150,10 +123,8 @@ Partial Class InfoSaveResults
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents PrintDialog1 As System.Windows.Forms.PrintDialog
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
     Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
-    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
