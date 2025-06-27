7ed04fc6e05c08a6933b90be54e36220cc78b2a4
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-06-27 09:01:35 +0200

    [REL] Update What's New section

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -1981,7 +1981,7 @@ Namespace My.Resources
         '''<summary>
         '''  Busca una cadena traducida similar a To get more information about what&apos;s new in this release, check out the release notes over at:
         '''
-        '''https://github.com/CodingWonders/DISMTools/releases/tag/v0.6.2_upd1.
+        '''https://github.com/CodingWonders/DISMTools/releases/tag/v0.6.2_upd2.
         '''</summary>
         Friend ReadOnly Property WhatsNew() As String
             Get
