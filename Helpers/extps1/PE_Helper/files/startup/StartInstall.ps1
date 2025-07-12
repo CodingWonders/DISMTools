ac0f84331e5aefcd4737c66a7f5605f765351640
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-07-12 15:03:48 +0200

    [PE Helper] Add Menu (part 4)

--- Helpers/extps1/PE_Helper/files/startup/StartInstall.ps1
+++ Helpers/extps1/PE_Helper/files/startup/StartInstall.ps1
@@ -4,4 +4,5 @@ $setupScript[5] = "set debug=0"
 Set-Content -Path "$env:SYSTEMDRIVE\Windows\system32\startnet.cmd" -Value $setupScript -Force
 Clear-Host
 Set-Location "$env:SYSTEMDRIVE\"
+Remove-Item -Path "$env:SYSTEMDRIVE\cmdcons" -Force | Out-Null
 iex "$env:SYSTEMDRIVE\Windows\system32\startnet.cmd"
