826eefbd3607a78eef6d9c2bb0cfb443457327c5
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-08-13 13:45:24 +0200

    [PE Helper/HotInstall/DSC] Remove non-WQL functions

--- Helpers/extps1/PE_Helper/tools/HotInstall/Installer/DSC/DiskSpaceChecker.vb
+++ Helpers/extps1/PE_Helper/tools/HotInstall/Installer/DSC/DiskSpaceChecker.vb
@@ -12,86 +12,6 @@ Public Class DiskSpaceChecker
     Dim isTestApplicable As Boolean
     Dim sourcePath As String = ""
 
-#Region "Non-WQL Drive Information Functions/Methods"
-
-    Function GetSystemDrives() As DriveInfo()
-        Return DriveInfo.GetDrives()
-    End Function
-
-    Function GetSystemBootDrive(FixedDrives As DriveInfo()) As DriveInfo
-        If FixedDrives Is Nothing Then Return Nothing
-        For Each FixedDrive As DriveInfo In FixedDrives
-            If FixedDrive.Name = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.Windows)) Then
-                Return FixedDrive
-            End If
-        Next
-        Return Nothing
-    End Function
-
-    Sub ListObtainedDisks(FixedDrives As DriveInfo(), RemovableDrives As DriveInfo())
-        reportContents &= "List of drives present in the host system:" & CrLf &
-                          "- Fixed drives: " & FixedDrives.Length & CrLf &
-                          "- Removable drives: " & RemovableDrives.Length & CrLf & CrLf
-
-        If FixedDrives.Length > 0 Then
-            reportContents &= "Information about the fixed drives:" & CrLf & CrLf
-            For Each FixedDrive As DriveInfo In FixedDrives
-                reportContents &= "- Drive letter: " & FixedDrive.Name & CrLf &
-                                  "- Drive type: " & FixedDrive.DriveType.ToString() & CrLf &
-                                  "- Drive format: " & FixedDrive.DriveFormat & CrLf &
-                                  "- Drive label: " & FixedDrive.VolumeLabel & CrLf &
-                                  "- Drive size: " & FixedDrive.TotalSize & " bytes (~" & Converters.BytesToReadableSize(FixedDrive.TotalSize) & ")" & CrLf &
-                                  "- Free space: " & FixedDrive.TotalFreeSpace & " bytes (~" & Converters.BytesToReadableSize(FixedDrive.TotalFreeSpace) & ")" & CrLf &
-                                  "- Is the system drive? " & If(FixedDrive.Name = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.Windows)), "Yes", "No") & CrLf & CrLf
-            Next
-        End If
-
-        If RemovableDrives.Length > 0 Then
-            reportContents &= "Information about the removable drives:" & CrLf & CrLf
-            For Each RemovableDrive As DriveInfo In RemovableDrives
-                reportContents &= "- Drive letter: " & RemovableDrive.Name & CrLf &
-                                  "- Drive type: " & RemovableDrive.DriveType.ToString() & CrLf &
-                                  "- Drive format: " & RemovableDrive.DriveFormat & CrLf &
-                                  "- Drive label: " & RemovableDrive.VolumeLabel & CrLf &
-                                  "- Drive size: " & RemovableDrive.TotalSize & " bytes (~" & Converters.BytesToReadableSize(RemovableDrive.TotalSize) & ")" & CrLf &
-                                  "- Free space: " & RemovableDrive.TotalFreeSpace & " bytes (~" & Converters.BytesToReadableSize(RemovableDrive.TotalFreeSpace) & ")" & CrLf & CrLf &
-                                  "- Is the system drive? " & If(RemovableDrive.Name = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.Windows)), "Yes", "No") & CrLf & CrLf
-            Next
-        End If
-
-        If Not FixedDrives.Any() And RemovableDrives.Any() Then
-            reportContents &= "All drives were detected to be removable" & CrLf & CrLf
-        End If
-    End Sub
-
-    Sub ListSpaceComparison(ImageNames As List(Of String), ImageSizes As List(Of Long), FixedDrives As DriveInfo())
-        If ImageNames.Count = 0 Then
-            Throw New Exception("No names have been passed")
-        End If
-        If ImageSizes.Count = 0 Then
-            Throw New Exception("No sizes have been passed")
-        End If
-        If FixedDrives.Length = 0 Then
-            Throw New Exception("No fixed drives have been passed")
-        End If
-
-        reportContents &= "Comparison of sizes:" & CrLf & CrLf
-
-        For Each FixedDrive As DriveInfo In FixedDrives
-            reportContents &= "- Drive, with label " & Quote & FixedDrive.VolumeLabel & Quote & " (" & FixedDrive.Name & "):" & CrLf
-            For x = 0 To ImageSizes.Count - 1
-                If FixedDrive.TotalSize > ImageSizes(x) Then
-                    reportContents &= "  - " & Quote & ImageNames(x) & Quote & " (index " & x + 1 & ") can be installed on this drive because there is enough free space." & CrLf
-                Else
-                    reportContents &= "  - " & Quote & ImageNames(x) & Quote & " (index " & x + 1 & ") cannot be installed on this drive because there is not enough free space." & CrLf
-                End If
-            Next
-        Next
-        reportContents &= CrLf
-    End Sub
-
-#End Region
-
 #Region "WQL Drive Information Functions/Methods"
 
     Sub ListObtainedDisks(DriveObjects As ManagementObjectCollection)
@@ -183,7 +103,7 @@ Public Class DiskSpaceChecker
         End Try
     End Sub
 
-    Sub GenerateDSCReportUsingWQL()
+    Sub GenerateDSCReport()
         Dim SystemDrives As ManagementObjectCollection = GetResultsFromManagementQuery("SELECT * FROM Win32_LogicalDisk WHERE DriveType = 3")        ' DriveType = 3 --> Local Disk
         If SystemDrives IsNot Nothing AndAlso SystemDrives.Count > 0 Then
             ' List the disks with a management query
@@ -226,65 +146,6 @@ Public Class DiskSpaceChecker
         End If
     End Sub
 
-    Sub GenerateDSCReport()
-        Dim SystemDrives() As DriveInfo = GetSystemDrives()
-        If SystemDrives.Length > 0 Then
-            ' Only show drives that we have access to (are ready)
-            SystemDrives = SystemDrives.Where(Function(drive) drive.IsReady).ToArray()
-            If SystemDrives.Any() Then
-                Dim FixedDrives() As DriveInfo = SystemDrives.Where(Function(drive) drive.DriveType = DriveType.Fixed).ToArray()
-                Dim RemovableDrives() As DriveInfo = SystemDrives.Where(Function(drive) drive.DriveType = DriveType.Removable).ToArray()
-
-                ' Report information about the drives
-                ListObtainedDisks(FixedDrives, RemovableDrives)
-
-                If Not FixedDrives.Any() And RemovableDrives.Any() Then
-                    Throw New Exception("Available drives were found, but they are all removable. This is not well supported..")
-                End If
-
-                ' Continue with the fixed drives
-                If FixedDrives.Any() Then
-                    Dim SystemBootDrive As DriveInfo = GetSystemBootDrive(FixedDrives)
-                    If SystemBootDrive IsNot Nothing Then
-                        ' We have grabbed the system boot drive
-                        progressMessage = "Getting size of disc image files..."
-                        BackgroundWorker1.ReportProgress(10)
-                        Dim FolderSize As Long = GetDirectorySize(sourcePath, "")
-                        ' Get the free space of the system boot drive, since we'll copy the files there
-                        Dim FreeSpaceOnSystemDrive As Long = SystemBootDrive.TotalFreeSpace
-                        ListFreeSpace(FreeSpaceOnSystemDrive, FolderSize)
-                        ' Check if we have enough space
-                        If FreeSpaceOnSystemDrive < FolderSize Then
-                            Throw New Exception("There is not enough space to copy the disc image files to the system drive. Please free up some space and try again.")
-                        End If
-                        ' Get information about the installation image and compare the expanded sizes of all indexes with the total space of all fixed drives
-                        progressMessage = "Getting image file information..."
-                        BackgroundWorker1.ReportProgress(40)
-                        Dim imgInfoCollection As DismImageInfoCollection = GetImageInformation(Path.Combine(sourcePath, "sources", "install.wim"))
-                        If imgInfoCollection IsNot Nothing Then
-                            progressMessage = "Getting image names and sizes..."
-                            BackgroundWorker1.ReportProgress(60)
-                            ' Grab the image names and expanded sizes
-                            Dim ImageNames As New List(Of String)
-                            Dim ImageSizes As New List(Of Long)
-                            For Each imgInfo As DismImageInfo In imgInfoCollection
-                                ImageNames.Add(imgInfo.ImageName)
-                                ImageSizes.Add(imgInfo.ImageSize)
-                            Next
-                            progressMessage = "Comparing image sizes with free space..."
-                            BackgroundWorker1.ReportProgress(80)
-                            ListSpaceComparison(ImageNames, ImageSizes, FixedDrives)
-                        End If
-                    End If
-                End If
-            Else
-                Throw New Exception("We could not detect available drives in your system.")
-            End If
-        Else
-            Throw New Exception("We could not detect available drives in your system.")
-        End If
-    End Sub
-
     Function GetImageInformation(ImagePath As String) As DismImageInfoCollection
         Dim imgInfoCollection As DismImageInfoCollection = Nothing
         Try
@@ -308,11 +169,7 @@ Public Class DiskSpaceChecker
         InitializeReport()
         progressMessage = GetValueFromLanguageData("DiskSpaceChecker.DSC_GetSysDrives")
         BackgroundWorker1.ReportProgress(5)
-        Try
-            GenerateDSCReportUsingWQL()     ' Let's make my database teacher happy!
-        Catch ex As Exception
-            GenerateDSCReport()
-        End Try
+        GenerateDSCReport()     ' Let's make my database teacher happy!
     End Sub
 
     Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
