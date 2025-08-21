2fd661b6882da72e6c5e54951fe6dd855161d06f
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-08-21 20:02:59 +0200

    [Unattended answer file/ADDS] Add to-be-used LDAP rep./OU-User mapping functions

--- Elements/AutoUnattend/ActiveDirectory/DomainServicesModule.vb
+++ Elements/AutoUnattend/ActiveDirectory/DomainServicesModule.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Runtime.InteropServices
+Imports System.DirectoryServices
+Imports System.DirectoryServices.AccountManagement
 Imports System.DirectoryServices.ActiveDirectory.Domain
 
 Module DomainServicesModule
@@ -156,6 +158,67 @@ Module DomainServicesModule
             DynaLog.LogMessage("NETAPI::NetWkstaGetInfo failed.")
         End If
         Return netbiosName
+    End Function
+
+    ''' <summary>
+    ''' Gets a mapping between organizational units in a domain and the users in each OU
+    ''' </summary>
+    ''' <param name="dsDomainDnsName">The name of the domain in DNS (eg: dismtools.local)</param>
+    ''' <param name="dsDomainControllerNetBios">The NetBIOS name of the domain controller (eg: DISMTOOLS-DC)</param>
+    ''' <returns>The mapping between organizational units and users in each OU</returns>
+    ''' <remarks>
+    ''' Unimplemented in 0.7, to be implemented in a future release of DISMTools. Keys are the organizational unit names,
+    ''' and values are all the users in each of the OUs. To access the value, use the organizational unit name as the key.
+    ''' Do note that this ONLY works with organizational units in a domain, and NOT sites.
+    ''' </remarks>
+    Public Function DSMapOrganizationalUnitsAndUsers(dsDomainDnsName As String, dsDomainControllerNetBios As String) As Dictionary(Of String, List(Of Principal))
+        Dim principalMappings As New Dictionary(Of String, List(Of Principal))
+        Dim orgUnitPaths As New List(Of String), orgUnits As New List(Of String)
+
+        Dim ldapPath As String = GetLdapPathFromDnsName(dsDomainDnsName)
+
+        Dim startingPoint As DirectoryEntry = New DirectoryEntry(String.Format("LDAP://{0}", ldapPath))
+        Dim searcher As DirectorySearcher = New DirectorySearcher(startingPoint)
+        searcher.Filter = "(objectCategory=organizationalUnit)"
+
+        For Each result As SearchResult In searcher.FindAll()
+            orgUnitPaths.Add(result.Path)
+            orgUnits.Add(result.GetDirectoryEntry().Properties("ou").Value.ToString())
+        Next
+
+        searcher.Dispose()      ' We need to call this due to some implementation quirks in the directory searcher
+
+        For i = 0 To orgUnits.Count
+            Dim allUsers As New List(Of String)
+            Dim ctx As PrincipalContext = New PrincipalContext(ContextType.Domain, dsDomainControllerNetBios, orgUnitPaths(i).Replace("LDAP://", ""))
+            Dim qbeUser As UserPrincipal = New UserPrincipal(ctx)
+
+            Dim srch As PrincipalSearcher = New PrincipalSearcher(qbeUser)
+            Dim principals As New List(Of Principal)
+            principals = srch.FindAll().ToList()
+
+            If principals.Count > 0 Then
+                principalMappings.Add(orgUnits(i), principals)
+            End If
+        Next
+
+        Return principalMappings
+    End Function
+
+    ''' <summary>
+    ''' Gets a Lightweight Directory Access Protocol (LDAP) representation of the domain name in DNS records.
+    ''' </summary>
+    ''' <param name="dnsName">The name of the domain in DNS records</param>
+    ''' <returns>A LDAP representation</returns>
+    ''' <remarks>LDAP representation of, for example, dismtools.local: DC=dismtools, DC=local</remarks>
+    Private Function GetLdapPathFromDnsName(dnsName As String) As String
+        Dim pathParts() As String = dnsName.Split(".")
+
+        For i = 0 To pathParts.Length
+            pathParts(i) = String.Format("DC={0}", pathParts(i))
+        Next
+
+        Return String.Join(",", pathParts)
     End Function
 
 End Module
