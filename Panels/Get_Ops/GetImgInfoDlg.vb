5879aeec91d3dc756d756bf153b55a4768b49201
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-01-23 20:41:25 +0100

    [Fix] Fix split container on HiDPI

--- Panels/Get_Ops/GetImgInfoDlg.vb
+++ Panels/Get_Ops/GetImgInfoDlg.vb
@@ -349,7 +349,9 @@ Public Class GetImgInfoDlg
         TextBox1.ForeColor = ForeColor
         ListView1.ForeColor = ForeColor
         LanguageList.ForeColor = ForeColor
-        SplitContainer2.SplitterDistance = WindowHelper.ScaleLogical(SplitContainer2.SplitterDistance)
+        If SplitContainer2.SplitterDistance = 440 Then
+            SplitContainer2.SplitterDistance = WindowHelper.ScaleLogical(SplitContainer2.SplitterDistance)
+        End If
 
         Dim handle As IntPtr = WindowHelper.GetWindowHandle(Me)
         WindowHelper.ToggleDarkTitleBar(handle, CurrentTheme.IsDark)
