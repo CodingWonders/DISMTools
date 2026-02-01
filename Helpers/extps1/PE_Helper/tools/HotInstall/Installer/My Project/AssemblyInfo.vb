05545b8e225c082cafd9db76222b8cef9b463d2f
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-02-01 14:54:51 +0100

    [PE Helper/HotInstall] Detect {ramdiskoptions} BCD object

--- Helpers/extps1/PE_Helper/tools/HotInstall/Installer/My Project/AssemblyInfo.vb
+++ Helpers/extps1/PE_Helper/tools/HotInstall/Installer/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el carácter '*', como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.7.2.0")> 
-<Assembly: AssemblyFileVersion("0.7.2.0")> 
+<Assembly: AssemblyVersion("0.7.3.0")> 
+<Assembly: AssemblyFileVersion("0.7.3.0")> 
