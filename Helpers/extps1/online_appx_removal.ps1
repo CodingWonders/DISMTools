cf37f4f9b9869adfa64a98d1988825c947dfea84
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-03-26 13:06:38 +0100

    [Enhance] Add future script for online appx package removal

---
+++ Helpers/extps1/online_appx_removal.ps1
@@ -0,0 +1,7 @@
+﻿param (
+    [Parameter(Mandatory = $true, Position = 0)] [string[]] $appxFullNames
+)
+
+Get-AppxPackage -AllUsers | Where-Object { $appxFullNames.Contains($_.PackageFullName) } | Remove-AppxPackage -AllUsers
+Write-Host "Log off and log on again for applications to be fully deprovisioned."
+Start-Sleep -Seconds 5
