5faaf37e88ac7369bac14f9b528e62c2c69a45e7
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-04-03 11:22:36 +0200

    [Enhance] Improve reliability of online AppX removal

---
+++ Helpers/extps1/online_appx_removal.ps1
@@ -0,0 +1,9 @@
+﻿param (
+    [Parameter(Mandatory = $true, Position = 0)] [string] $appxFullNames
+)
+
+$appxFullNamesArray = $appxFullNames.Split(";")
+Write-Host "Proceeding with the removal of $($appxFullNamesArray.Count) package(s)..."
+Get-AppxPackage -AllUsers | Where-Object { $appxFullNamesArray.Contains($_.PackageFullName) } | Remove-AppxPackage -AllUsers
+Write-Host "Log off and log on again for applications to be fully deprovisioned."
+Start-Sleep -Seconds 5
