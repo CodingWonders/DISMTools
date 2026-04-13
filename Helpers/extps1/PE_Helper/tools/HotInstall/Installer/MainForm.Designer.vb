d5d6bc15184d5d2c1b9b4d6caf3d3769540501b4
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-04-13 20:43:05 +0200

    [PE Helper/HotInstall] Add HiDPI support

--- Helpers/extps1/PE_Helper/tools/HotInstall/Installer/MainForm.Designer.vb
+++ Helpers/extps1/PE_Helper/tools/HotInstall/Installer/MainForm.Designer.vb
@@ -1028,8 +1028,8 @@ Partial Class MainForm
         '
         'MainForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(784, 561)
         Me.Controls.Add(Me.PageContainerPanel)
