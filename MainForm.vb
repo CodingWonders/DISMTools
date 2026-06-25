399a055b811129b69b97e86cf35e33a09616c57b
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-06-25 22:56:33 +0200

    Prepare for stable 0.8 version

--- MainForm.vb
+++ MainForm.vb
@@ -104,7 +104,7 @@ Public Class MainForm
     Public isSqlServerDTProj As Boolean
 
     ' Set branch name and codenames
-    Public dtBranch As String = "dt_pre_0.8_relcndid"
+    Public dtBranch As String = "stable"
     Public dt_codeName As String = "Infinity"
 
     ' Arrays and other variables used on background processes
