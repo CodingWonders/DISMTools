16210c77c7cb2a107e9f67f98a5fe070724e86a1
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-07-15 21:58:21 +0200

    [DynaLog] Update init

--- MainForm.vb
+++ MainForm.vb
@@ -623,8 +623,9 @@ Public Class MainForm
         DynaLog.LogMessage("- DISMTools Theme Designer: (c) " & GetCopyrightTimespan(2025, Date.Now.Year) & " CodingWonders Software")
         DynaLog.LogMessage("- Starter Script Editor: (c) " & GetCopyrightTimespan(2026, Date.Now.Year) & " CodingWonders Software" & CrLf &
                            "  Starter Script Library: (c) " & GetCopyrightTimespan(2026, Date.Now.Year) & " CodingWonders Software. Testing made by Abs and DaleCooper")
+        DynaLog.LogMessage("- BitLocker Drive Encryption Managed Library (BDELib): (c) " & GetCopyrightTimespan(2026, Date.Now.Year) & " CodingWonders Software")
         DynaLog.BeginLogging()
-        DynaLog.LogMessage("-------- Powered by CONTEMPOR/\NE\/S Wave 1 PREVIEW 2 --------")
+        DynaLog.LogMessage("-------- Powered by CONTEMPOR/\NE\/S --------")
     End Sub
 
     Private Async Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
