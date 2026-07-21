16147951775c5269878dcd1056c01052d8b77fd4
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-07-21 20:15:24 +0200

    [Enhance] Select source index by clicking list view

--- Panels/Img_Ops/ImgExport.Designer.vb
+++ Panels/Img_Ops/ImgExport.Designer.vb
@@ -467,6 +467,7 @@ Partial Class ImgExport
         Me.ListView1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.FullRowSelect = True
         Me.ListView1.Location = New System.Drawing.Point(16, 58)
         Me.ListView1.Name = "ListView1"
         Me.ListView1.Size = New System.Drawing.Size(450, 262)
