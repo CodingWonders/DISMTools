753025dfaf664874a240c58b727962958b8cb823
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-07-29 07:53:08 +0200

    Final commits for 0.8 Update 1
    The lot

--- Panels/Exe_Ops/PrgAbout.vb
+++ Panels/Exe_Ops/PrgAbout.vb
@@ -655,6 +655,7 @@ Public Class PrgAbout
             Exit Sub
         End Try
         If File.Exists(Application.StartupPath & "\update.exe") Then Process.Start(Application.StartupPath & "\update.exe", "/" & MainForm.dtBranch & " /pid=" & Process.GetCurrentProcess().Id)
+        OK_Button.PerformClick()
     End Sub
 
     Private Sub PictureBox5_MouseHover(sender As Object, e As EventArgs) Handles PictureBox5.MouseHover
