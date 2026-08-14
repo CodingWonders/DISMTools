ea406905386419603d3991addb619afb3b635626
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-08-14 17:18:15 +0200

    Update assembly information

--- MainForm.vb
+++ MainForm.vb
@@ -106,7 +106,7 @@ Public Class MainForm
     Public isSqlServerDTProj As Boolean
 
     ' Set branch name and codenames
-    Public dtBranch As String = "dt_pre_infinity_mk2"
+    Public dtBranch As String = "dt_pre_infinity_mk2_relcndid"
     Public dt_codeName As String = "InfinityMk2"
 
     ' Arrays and other variables used on background processes
--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.8.1.26082")> 
-<Assembly: AssemblyFileVersion("0.8.1.26082")> 
+<Assembly: AssemblyVersion("0.8.1.26083")> 
+<Assembly: AssemblyFileVersion("0.8.1.26083")> 
